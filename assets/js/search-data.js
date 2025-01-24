0bd5f46390b1773bc8b2b38bebd47a4a5102bbcc
lucasgautheron <lucas.gautheron@gmail.com>
2025-01-24 23:38:27 +0000

    Deploying to gh-pages from @ lucasgautheron-tests/lucasgautheron-tests.github.io@0217e3035e8daecbcbe48e758db3e6fe72442e33 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,51 +88,26 @@ ninja.data = [{
           window.location.href = "/posts/2018/05/lesprit-public-une-emission-privatisee-sur-france-culture/";
         
       },
-    },{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+    },{id: "projects-history-of-cosmology",
+          title: 'History of Cosmology',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-n-body-simulation-for-the-solar-system",
+          title: 'N-body simulation for the solar system',
+          description: "An educative programfor predicting and visualizing planetary motion that I created in high-school.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-rocket-science",
+          title: 'Rocket science',
+          description: "Automated water-rocket launch-pad (high-school project)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-optimizing-battery-charge-for-reduced-carbon-dioxyde-emissions",
+          title: 'Optimizing battery charge for reduced carbon dioxyde emissions',
+          description: "Includes electronics, microcontrollers, system modeling, convex optimization",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
